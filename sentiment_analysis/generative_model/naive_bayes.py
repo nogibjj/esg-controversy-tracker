37b21fccdc00895ebecb770a6e9edae2e24b5510
adityaemmanuel <40785524+adityaemmanuel@users.noreply.github.com>
2022-12-13 23:37:54 +0000

    generated synthetic data

--- sentiment_analysis/generative_model/naive_bayes.py
+++ sentiment_analysis/generative_model/naive_bayes.py
@@ -1,21 +1,27 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 import sys
 from datetime import datetime as dt
 import random
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
 sys.path.append('../..')
 from pre_processing_scripts import pre_processing
+
 random_state=12321
 dataset_path = '/workspaces/esg-controversy-tracker/dataset/news_sentiment.csv'
 
 # Limit the dataset to n rows
 data = pd.read_csv(dataset_path)
-max_class_samples = 10000
+data['confidence'] = data['confidence'].abs()
+data = data[data['confidence'] >= 0.99]
+#print(data['sentiment'].value_counts())
+max_class_samples = 25000
 data = data.sample(frac=1, random_state=random_state).reset_index()
 pos_sample = data[data['sentiment'] == 'POSITIVE'][0:max_class_samples]
 neg_sample = data[data['sentiment'] == 'NEGATIVE'][0:max_class_samples]
@@ -72,17 +78,18 @@ def generate_synthetic_data(naive_bayes_model, vocabulary):
     # Generating Negative Samples - class 0
     neg_sentences = []
     for n in range((total_samples_required//2)):
-        word_list = random.choices(Vocabulary, prior_word_prob[0], k=random.randint(5, 15))
+        word_list = random.choices(vocabulary, prior_word_prob[0], k=random.randint(5, 15))
         neg_sentences.append(" ".join(word_list))
     neg_df = pd.DataFrame({"Title":neg_sentences, "sentiment":0})
     synthetic_data = pd.concat([pos_df, neg_df])
+    synthetic_data.to_csv(f'synthetic_data_{dt.now().strftime("%m_%d_%Y_%H_%M_%S")}.csv')
 
     return synthetic_data
 
 
-train_x, test_x, train_y, test_y, Vocabulary = data_preprocess(data)
+train_x, test_x, train_y, test_y, vocabulary = data_preprocess(data)
 model = train_model(train_x, test_x, train_y, test_y)
-synthetic_data = generate_synthetic_data(model, Vocabulary)
+synthetic_data = generate_synthetic_data(model, vocabulary)
 
-train_x, test_x, train_y, test_y, Vocabulary = data_preprocess(synthetic_data)
+train_x, test_x, train_y, test_y, vocabulary = data_preprocess(synthetic_data)
 model = train_model(train_x, test_x, train_y, test_y)

21efad93989a7e742b727ed9cc8d3dbe8886a540
adityaemmanuel <40785524+adityaemmanuel@users.noreply.github.com>
2022-12-14 04:20:24 +0000

    formatted code

--- sentiment_analysis/discriminative_model/bert_fine_tuning.py
+++ sentiment_analysis/discriminative_model/bert_fine_tuning.py
@@ -1,89 +1,90 @@
 import pandas as pd
 import torch
-from transformers import AutoTokenizer,BertForSequenceClassification, TrainingArguments, Trainer
+from transformers import (
+    AutoTokenizer,
+    BertForSequenceClassification,
+    TrainingArguments,
+    Trainer,
+)
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
-random_state=12321
-dataset_path = '/workspaces/esg-controversy-tracker/dataset/news_sentiment.csv'
-device = torch.device('cuda')
+random_state = 12321
+dataset_path = "/workspaces/esg-controversy-tracker/dataset/news_sentiment.csv"
+device = torch.device("cuda")
 # Limit the dataset to n rows
 data = pd.read_csv(dataset_path)
-data['confidence'] = data['confidence'].abs()
-data = data[data['confidence'] >= 0.99]
+data["confidence"] = data["confidence"].abs()
+data = data[data["confidence"] >= 0.99]
 
 max_class_samples = 12800
 data = data.sample(frac=1, random_state=random_state).reset_index()
-pos_sample = data[data['sentiment'] == 'POSITIVE'][0:max_class_samples]
-neg_sample = data[data['sentiment'] == 'NEGATIVE'][0:max_class_samples]
+pos_sample = data[data["sentiment"] == "POSITIVE"][0:max_class_samples]
+neg_sample = data[data["sentiment"] == "NEGATIVE"][0:max_class_samples]
 dataset = pd.concat([pos_sample, neg_sample])
 
 
-dataset["sentiment"] = [0 if x=='NEGATIVE' else 1 for x in dataset['sentiment']]
+dataset["sentiment"] = [0 if x == "NEGATIVE" else 1 for x in dataset["sentiment"]]
 
 train_set = dataset[:9000]
 valid_set = dataset[9000:10000]
-test_set  = dataset[10000:]
+test_set = dataset[10000:]
 
 # Create The Dataset Class.
 class TheDataset(torch.utils.data.Dataset):
-
     def __init__(self, reviews, sentiments, tokenizer):
-        self.reviews    = reviews
+        self.reviews = reviews
         self.sentiments = sentiments
-        self.tokenizer  = tokenizer
-        self.max_len    = tokenizer.model_max_length
-  
+        self.tokenizer = tokenizer
+        self.max_len = tokenizer.model_max_length
+
     def __len__(self):
         return len(self.reviews)
-  
+
     def __getitem__(self, index):
         review = str(self.reviews[index])
         sentiments = self.sentiments[index]
 
         encoded_review = self.tokenizer.encode_plus(
             review,
-            add_special_tokens    = True,
-            max_length            = self.max_len,
-            return_token_type_ids = False,
-            return_attention_mask = True,
-            return_tensors        = "pt",
-            padding               = "max_length",
-            truncation            = True
+            add_special_tokens=True,
+            max_length=self.max_len,
+            return_token_type_ids=False,
+            return_attention_mask=True,
+            return_tensors="pt",
+            padding="max_length",
+            truncation=True,
         )
 
         return {
-            'input_ids': encoded_review['input_ids'][0],
-            'attention_mask': encoded_review['attention_mask'][0],
-            'labels': torch.tensor(sentiments, dtype=torch.long)
+            "input_ids": encoded_review["input_ids"][0],
+            "attention_mask": encoded_review["attention_mask"][0],
+            "labels": torch.tensor(sentiments, dtype=torch.long),
         }
 
+
 # Load the tokenizer for the BERT model.
-tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
 # Create Dataset objects for train/validation sets.
 train_set_dataset = TheDataset(
-    reviews    = train_set.Title.tolist(),
-    sentiments = train_set.sentiment.tolist(),
-    tokenizer  = tokenizer,
+    reviews=train_set.Title.tolist(),
+    sentiments=train_set.sentiment.tolist(),
+    tokenizer=tokenizer,
 )
 
 valid_set_dataset = TheDataset(
-    reviews    = valid_set.Title.tolist(),
-    sentiments = valid_set.sentiment.tolist(),
-    tokenizer  = tokenizer,
+    reviews=valid_set.Title.tolist(),
+    sentiments=valid_set.sentiment.tolist(),
+    tokenizer=tokenizer,
 )
 
 # Create DataLoader for train/validation sets.
 train_set_dataloader = torch.utils.data.DataLoader(
-    train_set_dataset,
-    batch_size  = 16,
-    num_workers = 4
+    train_set_dataset, batch_size=16, num_workers=4
 )
 
 valid_set_dataloader = torch.utils.data.DataLoader(
-    valid_set_dataset,
-    batch_size  = 16,
-    num_workers = 4
+    valid_set_dataset, batch_size=16, num_workers=4
 )
 
 # Get one batch as example.
@@ -91,68 +92,65 @@ train_data = next(iter(train_set_dataloader))
 valid_data = next(iter(valid_set_dataloader))
 
 # Print the output sizes.
-print( train_data["input_ids"].size(), valid_data["input_ids"].size() )
+print(train_data["input_ids"].size(), valid_data["input_ids"].size())
 
 model = BertForSequenceClassification.from_pretrained("bert-large-uncased")
 
 # Freeze the first 23 layers of the BERT
 for name, param in model.bert.named_parameters():
-    if ( not name.startswith('pooler') ) and "layer.23" not in name :
+    if (not name.startswith("pooler")) and "layer.23" not in name:
         param.requires_grad = False
+
 
 def compute_metrics(pred):
     labels = pred.label_ids
     preds = pred.predictions.argmax(-1)
-    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary')
+    precision, recall, f1, _ = precision_recall_fscore_support(
+        labels, preds, average="binary"
+    )
     acc = accuracy_score(labels, preds)
-    return {
-        'accuracy': acc,
-        'f1': f1,
-        'precision': precision,
-        'recall': recall
-    }
+    return {"accuracy": acc, "f1": f1, "precision": precision, "recall": recall}
 
 
 training_args = TrainingArguments(
-    output_dir                  = "./sentiment-analysis",
-    num_train_epochs            = 100,
-    per_device_train_batch_size = 128,
-    per_device_eval_batch_size  = 128,
-    warmup_steps                = 500,
-    weight_decay                = 0.01,
-    save_strategy               = "epoch",
-    evaluation_strategy         = "steps"
+    output_dir="./sentiment-analysis",
+    num_train_epochs=100,
+    per_device_train_batch_size=128,
+    per_device_eval_batch_size=128,
+    warmup_steps=500,
+    weight_decay=0.01,
+    save_strategy="epoch",
+    evaluation_strategy="steps",
 )
 
 trainer = Trainer(
-    model           = model,
-    args            = training_args,
-    train_dataset   = train_set_dataset,
-    eval_dataset    = valid_set_dataset,
-    compute_metrics = compute_metrics
+    model=model,
+    args=training_args,
+    train_dataset=train_set_dataset,
+    eval_dataset=valid_set_dataset,
+    compute_metrics=compute_metrics,
 )
 
 trainer.train()
 
 # Load the checkpoint
-model = BertForSequenceClassification.from_pretrained("./sentiment-analysis/checkpoint-100")
+model = BertForSequenceClassification.from_pretrained(
+    "./sentiment-analysis/checkpoint-100"
+)
 
 # Make the test set ready
 test_set_dataset = TheDataset(
-    reviews    = test_set.Title.tolist(),
-    sentiments = test_set.sentiment.tolist(),
-    tokenizer  = tokenizer,
+    reviews=test_set.Title.tolist(),
+    sentiments=test_set.sentiment.tolist(),
+    tokenizer=tokenizer,
 )
 
-training_args = TrainingArguments(
-    output_dir = "./sentiment-analysis",
-    do_predict = True
-)
+training_args = TrainingArguments(output_dir="./sentiment-analysis", do_predict=True)
 
 trainer = Trainer(
-    model           = model,
-    args            = training_args,
-    compute_metrics =compute_metrics,
+    model=model,
+    args=training_args,
+    compute_metrics=compute_metrics,
 )
 
 predictions = trainer.predict(test_set_dataset)
--- sentiment_analysis/generative_model/naive_bayes.py
+++ sentiment_analysis/generative_model/naive_bayes.py
@@ -10,24 +10,24 @@ from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-sys.path.append('../..')
+sys.path.append("../..")
 from pre_processing_scripts import pre_processing
 
-random_state=12321
-dataset_path = '/workspaces/esg-controversy-tracker/dataset/news_sentiment.csv'
+random_state = 12321
+dataset_path = "/workspaces/esg-controversy-tracker/dataset/news_sentiment.csv"
 
 # Limit the dataset to n rows
 data = pd.read_csv(dataset_path)
-data['confidence'] = data['confidence'].abs()
-data = data[data['confidence'] >= 0.99]
+data["confidence"] = data["confidence"].abs()
+data = data[data["confidence"] >= 0.99]
 
 max_class_samples = 25000
 data = data.sample(frac=1, random_state=random_state).reset_index()
-pos_sample = data[data['sentiment'] == 'POSITIVE'][0:max_class_samples]
-neg_sample = data[data['sentiment'] == 'NEGATIVE'][0:max_class_samples]
+pos_sample = data[data["sentiment"] == "POSITIVE"][0:max_class_samples]
+neg_sample = data[data["sentiment"] == "NEGATIVE"][0:max_class_samples]
 data = pd.concat([pos_sample, neg_sample])
 
-data["sentiment"] = [0 if x=='NEGATIVE' else 1 for x in data['sentiment']]
+data["sentiment"] = [0 if x == "NEGATIVE" else 1 for x in data["sentiment"]]
 
 
 def data_preprocess(data):
@@ -37,19 +37,21 @@ def data_preprocess(data):
     """
     # Pre-process Data
     data = data.sample(frac=1, random_state=random_state).reset_index()
-    data['Title'] = pre_processing.remove_stopwords(data['Title'])
-    data['Title'] = pre_processing.remove_non_alphanumeric(data['Title'])
-    data['Title'] = np.vectorize(pre_processing.stem_text)(data['Title'])
-    data['Title'] = pre_processing.remove_numbers(data['Title'])
+    data["Title"] = pre_processing.remove_stopwords(data["Title"])
+    data["Title"] = pre_processing.remove_non_alphanumeric(data["Title"])
+    data["Title"] = np.vectorize(pre_processing.stem_text)(data["Title"])
+    data["Title"] = pre_processing.remove_numbers(data["Title"])
 
     # Split the data into training and testing data
-    title = data['Title'].values
-    count_vec = CountVectorizer() 
+    title = data["Title"].values
+    count_vec = CountVectorizer()
     title = count_vec.fit_transform(title).toarray()
-    vocabulary = count_vec.get_feature_names_out() 
+    vocabulary = count_vec.get_feature_names_out()
 
-    labels = data['sentiment'].values
-    train_x, test_x, train_y, test_y = train_test_split(title, labels, stratify = labels, random_state=random_state)
+    labels = data["sentiment"].values
+    train_x, test_x, train_y, test_y = train_test_split(
+        title, labels, stratify=labels, random_state=random_state
+    )
     print(len(train_x), len(test_x))
     return train_x, test_x, train_y, test_y, vocabulary
 
@@ -64,6 +66,7 @@ def train_model(train_x, test_x, train_y, test_y, model_type=MultinomialNB()):
     print(accuracy_score(pred_y, test_y))
     return naive_bayes_model
 
+
 def generate_synthetic_data(naive_bayes_model, vocabulary):
 
     total_samples_required = len(data)
@@ -71,19 +74,25 @@ def generate_synthetic_data(naive_bayes_model, vocabulary):
 
     # Generating Positive Samples - class 1
     pos_sentences = []
-    for n in range((total_samples_required//2)):
-        word_list = random.choices(vocabulary, prior_word_prob[1], k=random.randint(5, 15))
+    for n in range((total_samples_required // 2)):
+        word_list = random.choices(
+            vocabulary, prior_word_prob[1], k=random.randint(5, 15)
+        )
         pos_sentences.append(" ".join(word_list))
-    pos_df = pd.DataFrame({"Title":pos_sentences, "sentiment":1})
+    pos_df = pd.DataFrame({"Title": pos_sentences, "sentiment": 1})
 
     # Generating Negative Samples - class 0
     neg_sentences = []
-    for n in range((total_samples_required//2)):
-        word_list = random.choices(vocabulary, prior_word_prob[0], k=random.randint(5, 15))
+    for n in range((total_samples_required // 2)):
+        word_list = random.choices(
+            vocabulary, prior_word_prob[0], k=random.randint(5, 15)
+        )
         neg_sentences.append(" ".join(word_list))
-    neg_df = pd.DataFrame({"Title":neg_sentences, "sentiment":0})
+    neg_df = pd.DataFrame({"Title": neg_sentences, "sentiment": 0})
     synthetic_data = pd.concat([pos_df, neg_df])
-    synthetic_data.to_csv(f'synthetic_data_{dt.now().strftime("%m_%d_%Y_%H_%M_%S")}.csv')
+    synthetic_data.to_csv(
+        f'synthetic_data_{dt.now().strftime("%m_%d_%Y_%H_%M_%S")}.csv'
+    )
 
     return synthetic_data
 
